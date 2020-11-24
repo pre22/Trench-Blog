93d9a626359449813bc7064808478d8d53e326a1
Precious Chika <43546663+pre22@users.noreply.github.com>
2020-11-24 22:03:57 +0100

    chore: updated blogs route

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import  path
-from blogs.views import BlogListView
+from blogs.views import BlogListView, AboutPageView, ContactPageView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
 ]
